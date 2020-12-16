2aa890897e50dff4a7fd9ae5630feb40eba80a52
Govi S Sasi <Govi S Sasi@DESKTOP-H5PFLS1.com>
2020-12-16 15:38:29 +0530

    Rectify Internal Server Error

--- restful_flask.py
+++ restful_flask.py
@@ -27,7 +27,7 @@ api.add_resource(Store,"/Store/<string:name>")
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(debug=True,port=5000)
+    app.run(debug=True)
 
 
 
